53985ec15556e447cb735e855ab5daa50629ee7b
PUGE <mail@puge.org>
2021-10-06 09:40:34 +0800

    save

--- dist/static/js/owo.core.js
+++ dist/static/js/owo.core.js
@@ -1,4 +1,4 @@
-// Thu Sep 30 2021 13:53:00 GMT+0800 (中国标准时间)
+// Wed Oct 06 2021 09:39:38 GMT+0800 (中国标准时间)
 var owo = {tool: {},state: {},event: {}};
 /* 方法合集 */
 var _owo = {
@@ -676,6 +676,11 @@ _owo.showPage = function() {
   // 取出URL地址判断当前所在页面
   var pageArg = _owo.getarg(window.location.hash)
   
+  if (pageArg !== null) {
+    window.location.href = ''
+    return
+  }
+  
   
 
   // 从配置项中取出程序入口
@@ -813,21 +818,4 @@ function switchPage (oldUrlParam, newUrlParam) {
 if (window.onhashchange) {window.onhashchange = _owo.hashchange;} else {window.onpopstate = _owo.hashchange;}
 // 执行页面加载完毕方法
 _owo.ready(_owo.showPage)
-
-
-// 这是用于代码调试的自动刷新代码，他不应该出现在正式上线版本!
-if ("WebSocket" in window) {
-  // 打开一个 web socket
-  if (!window._owo.ws) window._owo.ws = new WebSocket("ws://" + window.location.host)
-  window._owo.ws.onmessage = function (evt) { 
-    if (evt.data == 'reload') {
-      location.reload()
-    }
-  }
-  window._owo.ws.onclose = function() { 
-    console.info('与服务器断开连接')
-  }
-} else {
-  console.error('浏览器不支持WebSocket')
-}
 
--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ function scrollIntoView () {
 }
 
 // 阻止微信拖动
-document.body.addEventListener('touchmove', function (e) {
-  e.preventDefault() // 阻止默认的处理方式(阻止下拉滑动的效果)
-}, {passive: false})
+// document.body.addEventListener('touchmove', function (e) {
+//   e.preventDefault() // 阻止默认的处理方式(阻止下拉滑动的效果)
+// }, {passive: false})
 
 setTimeout(() => {
   autoScale({
@@ -17,3 +17,69 @@ setTimeout(() => {
     box: '.scale-box'
   })
 }, 100);
+
+var activeIndex = 0
+var timu = [
+  {
+    "type": "单选题",
+    "title": "中国共产党第一次全国代表大会先后在(    )召开。",
+    "answer": ["上海、嘉兴", "上海、天津", "北京、上海", "北京、广州"],
+    "right": 0
+  },
+  {
+    "type": "多选题",
+    "title": "中国共产党第一次全国代表大会先后在(    )召开。",
+    "answer": ["上海、嘉兴", "上海、天津", "北京、上海", "北京、广州"],
+    "right": [0, 1, 2]
+  },
+  {
+    "type": "判断题",
+    "title": "1928年6月在莫斯科举行的是中国共产党第五次全国代表大会。",
+    "answer": ["正确", "错误"],
+    "right": 0
+  },
+  {
+    "type": "填空题",
+    "title": "“政权是由枪杆子中取得的”这一论断是毛泽东在[输入]提出的。",
+    "right": "测试"
+  },
+  {
+    "type": "视频题",
+    "title": "<video controls='controls' src='https://cunchu.site/show/mp4/dandelion.mp4'></video>请看视频，然后回答下列问题：视频选自电影《鸡毛信》，请问小男主角海娃除了担任华北抗日根据地龙门村的儿童团长外，他还是一名_______。",
+    "answer": ["小邮差", "放牛娃", "放羊娃", "卖报员"],
+    "right": 0
+  },
+  {
+    "type": "音频题",
+    "title": "<audio controls='controls' src='https://tts-async-audio-gz-1300466766.cos.ap-guangzhou.myqcloud.com/20211006%2F1256763111%2Fgz-3a0fe2af-266d-474d-9341-edead424662e.mp3?sign=q-sign-algorithm%3Dsha1%26q-ak%3DAKIDJXcaKs2w4vZw5zTCrHRM7dOwre9Xk9MJ%26q-sign-time%3D1633474867%3B1634079667%26q-key-time%3D1633474867%3B1634079667%26q-header-list%3D%26q-url-param-list%3D%26q-signature%3Dfd4003c7368a8e396132e149355306911a1794c6'></audio>请听音频，然后回答下列问题：请问《义勇军进行曲》的曲作者、词作者分别是谁？",
+    "answer": ["聂耳、田汉", "聂耳、周扬", "聂耳、夏衍", "冼星海、田汉"],
+    "right": 0
+  }
+]
+
+function check (ind, el) {
+  if (timu[activeIndex].type == '填空题') {
+    if (document.querySelector('.title input').value.indexOf(timu[activeIndex].right) >= 0) {
+      owo.tool.toast('答对了！')
+    } else {
+      owo.tool.toast('回答错误')
+    }
+    document.querySelector('.title input').value = timu[activeIndex].right
+  } else if (timu[activeIndex].type == '多选题') {
+    el.classList.add('check')
+    timu[activeIndex].right.forEach(element => {
+      document.querySelectorAll('.answer-box .item')[element].classList.add('right')
+    });
+  } else {
+    el.classList.add('check')
+    document.querySelectorAll('.answer-box .item')[timu[activeIndex].right].classList.add('right')
+    if (timu[activeIndex].right != ind) {
+      owo.tool.toast('回答错误')
+    }
+  }
+  
+}
+
+function check2 (ind, el) {
+  el.classList.add('check')
+}
